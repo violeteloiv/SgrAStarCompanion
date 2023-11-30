5e2c4678e97bc6a1a4130018502a2c65c6008cd0
violeteloiv <violetisacoolcolor@gmail.com>
2023-11-30 12:32:46 -0800

    iEKL Start (Not Finished) & Eccentricity Constraints

--- eccentricity.py
+++ eccentricity.py
@@ -5,13 +5,13 @@ import time
 from modules.constants import *
 
 # Define The Range Of Masses
-separations = np.arange(1e-1, 1e2, 1)
+separations = np.arange(1e-1, 1e4, 1)
 
 # Define the plot itself.
 plt.title("Constraints on Sgr A* Companion Eccentricity")
 plt.xlabel("$a_c$ $[au]$")
 plt.ylabel("$e$")
-plt.axis([1e-1, 1e2, 0, 1.05])
+plt.axis([1e-1, 1e4, 0, 1.05])
 
 # Make the axes log scale.
 plt.semilogx()
@@ -20,5 +20,11 @@ plt.semilogx()
 separation_ratio = (SgrAttributes.SCHWARZSCHILD_RADIUS.value * Conversion.METERS_TO_AU.value) / separations
 eccentricities = (1 - separation_ratio) / (1 + separation_ratio)
 
-plt.plot(separations, eccentricities)
+plt.plot(separations, eccentricities, label="0.1 au")
+
+separation_ratio_1 = (10 * SgrAttributes.SCHWARZSCHILD_RADIUS.value * Conversion.METERS_TO_AU.value) / separations
+eccentricities_1 = (1 - separation_ratio_1) / (1 + separation_ratio_1)
+
+plt.plot(separations, eccentricities_1, label="1 au")
+
 plt.savefig("./output/eccentricities.png")
--- modules/iekl.py
+++ modules/iekl.py
@@ -12,17 +12,28 @@ def iekl_calculation(plt, masses, orbital_parameters, plot_options):
 
 	# base conversions
 	orbit_period = orbital_parameters["period"] * Conversion.YEARS_TO_SECONDS.value
+	proxy_masses = masses * Conversion.SOLAR_MASS_TO_KILOGRAMS.value
+	orbital_sm_axis = orbital_parameters["sm-axis"] * Conversion.AU_TO_METERS.value
 
-	# calculate the front constant of the expression.
-	front_constant = (4/3) * orbit_period * (1 - (orbital_parameters["eccentricity"] ** 2) ** 2)
-	print(front_constant)
+	# calculate the mass terms associated in the denominator
+	denominator_mass_terms = np.power(SgrAttributes.MASS.value + proxy_masses, 2) / (SgrAttributes.MASS.value * proxy_masses)
 
-	# calculate the mass terms associated
-	summed_masses_squared = np.power(masses + SgrAttributes.MASS.value, 2)
-	mass_terms = summed_masses_squared / (masses * SgrAttributes.MASS.value)
-	print(mass_terms)
-
-	# calculate general relativistic precession parameters
-
+	# calculate the denominator result
+	denominator = (8/3) * Math.PI.value * orbit_period * (1 - orbital_parameters["eccentricity"] ** 2) * denominator_mass_terms * (orbital_sm_axis ** 2)
+	print("iEKL", denominator)
 	
+	# calculate the numerator result
+	companion_eccentricity = 0.8 # this is sketchy !
+	numerator = (6 * Math.PI.value * (Fundamental.GRAV_CONST.value ** 2) * np.power(proxy_masses, 2)) / ((Fundamental.SPEED_OF_LIGHT.value ** 2) * (1 - companion_eccentricity ** 2))
+	print("iEKL", numerator)
 
+	# calculate the final value of semi-major axis in terms of companion masses
+	proxy_axis = np.power(numerator / denominator, 2)
+	result = proxy_axis * Conversion.METERS_TO_AU.value
+	print("a_c", result)
+
+	plt.plot(masses, result, 
+		label="$t_{iEKL} \sim t_{GR}$", 
+		linestyle=plot_options["style"], 
+		color=plot_options["color"]
+	)
